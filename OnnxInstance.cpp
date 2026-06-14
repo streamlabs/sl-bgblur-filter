dc904a2e1810a8e719e5e23850d307a2edaa4574
unknown <stevensoftware52@gmail.com>
2026-06-14 16:09:41 -0400

    Update OnnxInstance.cpp

--- OnnxInstance.cpp
+++ OnnxInstance.cpp
@@ -196,7 +196,8 @@ bool OnnxInstance::getRGBAFromStageSurface(gs_texrender_t*& texrender, gs_stages
 	if (!gs_stagesurface_map(stagesurface, &video_data, &linesize))
 		return false;
 
-	outputBGRA = cv::Mat(height, width, CV_8UC4, video_data, linesize).clone();
+	outputBGRA = cv::Mat(height, width, CV_8UC4, video_data, linesize);
+
 	gs_stagesurface_unmap(stagesurface);
 	return true;
 }
